af9564705222c07e21d474efacd5e91b9851bfeb
Aang Agus <91416171+angga268@users.noreply.github.com>
2024-03-06 22:35:34 +0700

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+}
